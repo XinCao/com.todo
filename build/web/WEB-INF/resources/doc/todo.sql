a6a2a83acfb419ce5bc561d0ae62e899f610b55f
曹鑫 <jingxin191314@foxmail.com>
2014-03-28 14:07:32 +0800

    add ignore

--- build/web/WEB-INF/resources/doc/todo.sql
+++
@@ -1,6 +0,0 @@
-create database todo character set utf8 collate utf8_general_ci;
-
-drop table if exists todo;
-CREATE TABLE `todo` (
-  `id` int(11) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
