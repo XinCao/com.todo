a283c9e28242fc61096162a13827e71eb6ff3a78
曹鑫 <jingxin191314@foxmail.com>
2014-04-14 16:58:16 +0800

    add cached

--- test/com/todo/mapper/UserMapperTest.java
+++ test/com/todo/mapper/UserMapperTest.java
@@ -2,8 +2,8 @@ package com.todo.mapper;
 
 import com.todo.model.User;
 import com.todo.service.ACService;
-import java.util.Date;
 import junit.framework.TestCase;
+import org.mybatis.caches.memcached.MemcachedCache;
 import org.springframework.context.ApplicationContext;
 
 /**
@@ -11,40 +11,32 @@ import org.springframework.context.ApplicationContext;
  * @author caoxin
  */
 public class UserMapperTest extends TestCase {
-    
+
     private ApplicationContext ac;
     private UserMapper userMapper;
-    
+    private static final String group = "mapper_id";
+    private MemcachedCache memcachedCache = new MemcachedCache(group);
+
     public UserMapperTest(String testName) {
         super(testName);
     }
-    
+
     @Override
     protected void setUp() throws Exception {
         super.setUp();
         this.ac = ACService.getAC();
         this.userMapper = ac.getBean(UserMapper.class);
     }
-    
+
     @Override
     protected void tearDown() throws Exception {
         super.tearDown();
     }
 
     public void testInsertUser() {
-
+        User user = new User();
+        this.userMapper.insertUser(user);
+        user = this.userMapper.selectUser(user.getAccount());
+        memcachedCache.getObject("");
     }
-
-//    public void testDeleteUser() {
-//      
-//    }
-//
-//    public void testUpdateUser() {
-//    }
-//
-//    public void testSelectUser() {
-//    }
-//
-//    public void testSelectUserList() {
-//    }
 }
