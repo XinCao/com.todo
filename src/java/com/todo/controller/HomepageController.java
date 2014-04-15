8dcbf7f15a523abb54a4ad44647c11bc353e2653
caoxin <jingxin191314@foxmail.com>
2014-04-16 00:51:56 +0800

    test logger

--- src/java/com/todo/controller/HomepageController.java
+++ src/java/com/todo/controller/HomepageController.java
@@ -1,10 +1,12 @@
 package com.todo.controller;
 
 import java.util.Map;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-
+ 
 /**
  * 主界面显示（产品的介绍）
  *
@@ -14,8 +16,11 @@ import org.springframework.web.bind.annotation.RequestMethod;
 @RequestMapping("/")
 public class HomepageController {
 
+    private static final Logger logger = LoggerFactory.getLogger(HomepageController.class);
+
     @RequestMapping(method = RequestMethod.GET)
     public String showAction(Map<String, Object> map) {
+        logger.debug("have people login homepage!");
         return "homepage";
     }
 }
