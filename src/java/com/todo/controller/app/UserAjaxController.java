a01f1129ac0631ccb6807f40a21f50ec75ccdeed
曹鑫 <jingxin191314@foxmail.com>
2014-04-18 13:18:53 +0800

    We have to make each other farther and farther

--- src/java/com/todo/controller/app/UserAjaxController.java
+++ src/java/com/todo/controller/app/UserAjaxController.java
@@ -1,11 +1,14 @@
 package com.todo.controller.app;
 
 import com.todo.model.User;
+import java.util.Map;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 /**
  *
@@ -18,7 +21,13 @@ public class UserAjaxController {
     private static final Logger logger = LoggerFactory.getLogger(UserAjaxController.class);
 
     @RequestMapping(value="login_check", method = RequestMethod.POST)
-    public User loginCheckAction() {
+    public @ResponseBody Map<String, Object> loginCheckAction(@RequestParam(value="account", required = true)String account, @RequestParam(value="password", required = true)String password) {
         return null;
     }
+
+    @RequestMapping(value="register", method = RequestMethod.POST)
+    public User registerAction() {
+        return null;
+    }
+    
 }
